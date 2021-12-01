94d0df62d64b5c77a83d612f96ec52f1c2330b64
Lecogoni <giraud.nicolas@me.com>
2021-12-01 14:56:23 +0000

    chat perche game word, cat change color when hover box - !! diagonale move not working

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 
 
-  
 const cat = document.getElementById('cat')
 const box = document.getElementById('box')
 
@@ -25,30 +24,36 @@ function displayDimensions() {
  
 
 let key = document.addEventListener('keydown', (event) => {
-  keynum = event.key;
-  setInterval(moveDiv(keynum), 500,)
+  moveDiv(event.key)
 });
 
 
-  function moveDiv(keynum){
+function moveDiv(keynum){
 
-    
-    if (keynum === 'ArrowRight'){
-      
-      cat.style.cssText = `left:${catLeft += 2}px;top:${catTop}px;`;
-      
-    } else if (keynum === 'ArrowDown') {
-      
-      cat.style.cssText = `top:${catTop += 2}px;left:${catLeft}px;`;
-    } else if (keynum === 'ArrowUp') {
-      
-      cat.style.cssText = `top:${catTop -= 2}px;left:${catLeft}px;`;
-    } else if (keynum === 'ArrowLeft') {
-      
-      cat.style.cssText = `left:${catLeft -= 2}px;top:${catTop}px;`;
-    }
+  if (keynum === 'ArrowRight'){
+    cat.style.cssText = `left:${catLeft += 5}px;top:${catTop}px;`;
+  } else if (keynum === 'ArrowDown') {
+    cat.style.cssText = `top:${catTop += 5}px;left:${catLeft}px;`;
+  } else if (keynum === 'ArrowUp') {
+    cat.style.cssText = `top:${catTop -= 5}px;left:${catLeft}px;`;
+  } else if (keynum === 'ArrowLeft') {
+    cat.style.cssText = `left:${catLeft -= 5}px;top:${catTop}px;`;
   }
-    
+  let catPos = cat.getBoundingClientRect();
+  checkMatch(catPos)
+}
 
+function checkMatch(catPos){
+  
+  let boxPos = box.getBoundingClientRect()
 
+  if ((catPos.top >= boxPos.top - 10 && catPos.left >= boxPos.left - 10) && (catPos.top <= boxPos.top + 10 && catPos.left <= boxPos.left + 10)) {
+    cat.classList.add("blue");
+  } else {
+    cat.classList.remove("blue");
+  }
 
+}
+
+// https://stackoverflow.com/questions/7298507/move-element-with-keypress-multiple
+// http://jsfiddle.net/fbFuW/220/
